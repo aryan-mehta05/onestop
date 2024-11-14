f096da9049e5084f62cf1160e7210f7458cbb576
phuongn1810 <phuonglinhn9@gmail.com>
2024-11-14 16:02:44 -0500

    initial landing page commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Routes, Route} from "react-router";
+import { HashRouter, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import LandingPage from "./LandingPage";
 
-export default App;
+export default function App() {
+ return (
+  <div>
+    <LandingPage />
+   
+  </div>
+);}
