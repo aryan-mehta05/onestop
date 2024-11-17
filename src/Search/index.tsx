2fa4b7c75a12bf1bdec2c5a3edccd1b6e771fdd0
Chris <cgormley1275@gmail.com>
2024-11-17 17:36:48 -0500

    Cleaned up naming and a few other things in search component code

--- src/Search/index.tsx
+++ src/Search/index.tsx
@@ -4,9 +4,11 @@ import * as client from "./client";
 export default function Search() {
     const [search_params, setSearch_params] = useState({ origin: "MAD", one_way: false, nonstop: false, max_price: 999999 });
     const [data, setData] = useState([{ "id": 1 }]);
-    const testAPI = async (origin: string, one_way: boolean, nonstop: boolean, max_price?: number) => {
+    const [data_loaded, setData_loaded] = useState(false);
+    const getFlightData = async (origin: string, one_way: boolean, nonstop: boolean, max_price?: number) => {
         try {
-            const response = await client.test_api(origin, one_way, nonstop, max_price);
+            const response = await client.get_flight_inspo_data(origin, one_way, nonstop, max_price);
+            setData_loaded(true);
             setData(response.data);
         } catch (error) {
             alert("Something went wrong.  Please ensure your origin is a valid Airport code.")
@@ -35,13 +37,13 @@ export default function Search() {
                     if (!search_params.origin) {
                         alert("Please select an origin.")
                     } else {
-                        testAPI(search_params.origin, search_params.one_way, search_params.nonstop, search_params.max_price)
+                        getFlightData(search_params.origin, search_params.one_way, search_params.nonstop, search_params.max_price)
                     }
                 }
-                }>alert</button>
+                }>Get Flight Insperation</button>
             </form>
             <ul>
-                {data.map((object: any) => (
+                {data_loaded && data.map((object: any) => (
                     <div>
                         <li>
                             <div>Origin: {object.origin}</div>
@@ -51,18 +53,9 @@ export default function Search() {
                             {object && object.price && <div>Price: {JSON.stringify(object.price.total, null, 2)}</div>}
                             <br />
                         </li>
-                        {/* <li>{JSON.stringify(object, null, 2)}</li> */}
                     </div>
                 ))}
             </ul>
         </div>
     )
 }
-
-// { "type": "flight-destination", "origin": "BOS", "destination": "LAX", "departureDate": "2024-12-10", "returnDate": "2024-12-11", "price": { "total": "188.20" }, "links": { "flightDates": "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=BOS&destination=LAX&departureDate=2024-11-18,2025-05-16&oneWay=false&duration=1,15&nonStop=false&viewBy=DURATION", "flightOffers": "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=LAX&departureDate=2024-12-10&returnDate=2024-12-11&adults=1&nonStop=false" } }
-// { "type": "flight-destination", "origin": "BOS", "destination": "LHR", "departureDate": "2025-01-18", "returnDate": "2025-01-19", "price": { "total": "321.31" }, "links": { "flightDates": "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=BOS&destination=LHR&departureDate=2024-11-18,2025-05-16&oneWay=false&duration=1,15&nonStop=false&viewBy=DURATION", "flightOffers": "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=LHR&departureDate=2025-01-18&returnDate=2025-01-19&adults=1&nonStop=false" } }
-// { "type": "flight-destination", "origin": "BOS", "destination": "CDG", "departureDate": "2024-12-02", "returnDate": "2024-12-08", "price": { "total": "360.31" }, "links": { "flightDates": "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=BOS&destination=CDG&departureDate=2024-11-18,2025-05-16&oneWay=false&duration=1,15&nonStop=false&viewBy=DURATION", "flightOffers": "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=CDG&departureDate=2024-12-02&returnDate=2024-12-08&adults=1&nonStop=false" } }
-// { "type": "flight-destination", "origin": "BOS", "destination": "DUB", "departureDate": "2025-01-07", "returnDate": "2025-01-11", "price": { "total": "377.91" }, "links": { "flightDates": "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=BOS&destination=DUB&departureDate=2024-11-18,2025-05-16&oneWay=false&duration=1,15&nonStop=false&viewBy=DURATION", "flightOffers": "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=DUB&departureDate=2025-01-07&returnDate=2025-01-11&adults=1&nonStop=false" } }
-// { "type": "flight-destination", "origin": "BOS", "destination": "BCN", "departureDate": "2025-01-12", "returnDate": "2025-01-17", "price": { "total": "386.21" }, "links": { "flightDates": "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=BOS&destination=BCN&departureDate=2024-11-18,2025-05-16&oneWay=false&duration=1,15&nonStop=false&viewBy=DURATION", "flightOffers": "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=BCN&departureDate=2025-01-12&returnDate=2025-01-17&adults=1&nonStop=false" } }
-// { "type": "flight-destination", "origin": "BOS", "destination": "FCO", "departureDate": "2025-01-16", "returnDate": "2025-01-23", "price": { "total": "415.71" }, "links": { "flightDates": "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=BOS&destination=FCO&departureDate=2024-11-18,2025-05-16&oneWay=false&duration=1,15&nonStop=false&viewBy=DURATION", "flightOffers": "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=FCO&departureDate=2025-01-16&returnDate=2025-01-23&adults=1&nonStop=false" } }
-// { "type": "flight-destination", "origin": "BOS", "destination": "MAD", "departureDate": "2025-01-19", "returnDate": "2025-01-31", "price": { "total": "418.51" }, "links": { "flightDates": "https://test.api.amadeus.com/v1/shopping/flight-dates?origin=BOS&destination=MAD&departureDate=2024-11-18,2025-05-16&oneWay=false&duration=1,15&nonStop=false&viewBy=DURATION", "flightOffers": "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=BOS&destinationLocationCode=MAD&departureDate=2025-01-19&returnDate=2025-01-31&adults=1&nonStop=false" } }
