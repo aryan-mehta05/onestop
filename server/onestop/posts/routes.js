5abe2dc5a7afe8e30be2d7d4b2279d43704dcf35
Chris <cgormley1275@gmail.com>
2024-11-30 20:44:50 -0500

    added a few additional routes

--- server/onestop/posts/routes.js
+++ server/onestop/posts/routes.js
@@ -1,3 +1,9 @@
 export default function PostsRoutes(app) {
     const createPost = async (req, res) => { };
+    const likePost = async (req, res) => { };
+    const updatePost = async (req, res) => { };
+    const deletePost = async (req, res) => { };
+    const getAllPosts = async (req, res) => { };
+    const getPostsByUser = async (req, res) => { };
+    const getPostsByLocation = async (req, res) => { };
 }
