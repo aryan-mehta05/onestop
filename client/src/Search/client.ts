b448966c5735b4a9d4b6c1d39a7958a247f33143
Chris <cgormley1275@gmail.com>
2024-12-02 20:19:14 -0500

    added external API call to search page to get city from airport code

--- client/src/Search/client.ts
+++ client/src/Search/client.ts
@@ -6,7 +6,7 @@ const API_SECRET = "GeQt2ukuf9LINVKD";
 const AUTH_API = "https://test.api.amadeus.com/v1/security/oauth2/token";
 
 // const FLIGHT_OFFERS_API = "https://test.api.amadeus.com/v2";
-const FLIGHT_INSPO_API = "https://test.api.amadeus.com/v1";
+const BASE_API = "https://test.api.amadeus.com/v1";
 // const test_parameters = `${FLIGHT_OFFERS_API}/shopping/flight-offers?originLocationCode=SYD&destinationLocationCode=BKK&departureDate=2024-12-01&adults=1&max=2`;
 // const test_parameters2 = `${FLIGHT_INSPO_API}/shopping/flight-destinations?origin=MAD&oneWay=false&nonStop=false&maxPrice=500`;
 
@@ -22,7 +22,14 @@ export const get_flight_inspo_data = async (origin: string, one_way: boolean, no
     const one_way_string = JSON.stringify(one_way)
     const nonstop_string = JSON.stringify(nonstop)
     const max_price_string = JSON.stringify(max_price)
-    const API_url = `${FLIGHT_INSPO_API}/shopping/flight-destinations?origin=${origin}&oneWay=${one_way_string}&nonStop=${nonstop_string}&maxPrice=${max_price ? max_price_string : "999999"}`
+    const API_url = `${BASE_API}/shopping/flight-destinations?origin=${origin}&oneWay=${one_way_string}&nonStop=${nonstop_string}&maxPrice=${max_price ? max_price_string : "999999"}`
+    const { data } = await axios.get(API_url, { headers: { 'Authorization': `Bearer ${token}` } });
+    return data;
+}
+
+export const get_airport_city_details = async (airport_code: string) => {
+    const token = await get_token();
+    const API_url = `${BASE_API}/reference-data/locations?subType=CITY&keyword=${airport_code}&sort=analytics.travelers.score&view=LIGHT}`
     const { data } = await axios.get(API_url, { headers: { 'Authorization': `Bearer ${token}` } });
     return data;
 }
